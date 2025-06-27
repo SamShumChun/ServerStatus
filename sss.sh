91ea6f878f8f24343420f739f0f9ec310e745ad6
Sam Shum <38761558+SamShumChun@users.noreply.github.com>
2025-06-27 08:57:12 +0800

    Update sss.sh

--- sss.sh
+++ sss.sh
@@ -4,7 +4,7 @@
 #   System Required: CentOS 7+ / Debian 8+ / Ubuntu 16+ /
 #     Arch 未测试
 #   Description: Server Status 监控安装脚本
-#   Github: https://github.com/lidalao/ServerStatus
+#   Github: https://github.com/SamShumChun/ServerStatus
 #========================================================
 
 GITHUB_RAW_URL="https://raw.githubusercontent.com/SamShumChun/ServerStatus/master"
